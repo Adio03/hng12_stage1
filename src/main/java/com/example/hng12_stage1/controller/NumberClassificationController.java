746683d0414f85ccb4b18af0d734a1fbc62e59ba
Adio03 <adioldmj@gmail.com>
2025-02-05 14:34:53 +0100

    number classification api

--- src/main/java/com/example/hng12_stage1/controller/NumberClassificationController.java
+++ src/main/java/com/example/hng12_stage1/controller/NumberClassificationController.java
@@ -1,6 +1,7 @@
 package com.example.hng12_stage1.controller;
 
 import com.example.hng12_stage1.service.NumberClassificationService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
